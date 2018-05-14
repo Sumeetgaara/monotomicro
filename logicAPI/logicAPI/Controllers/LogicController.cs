50bc2810c1c40285eb74de8e9dcd897fb8f92216
sumbagaara <sumbagaara@gmail.com>
2018-05-15 00:39:56 +0530

    added

---
+++ logicAPI/logicAPI/Controllers/LogicController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace logicAPI.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[controller]")]
+    public class LogicController : Controller
+    {
+        [Route("getallAsync")]
+        [HttpGet]
+        public async Task<List<Stock>> getallAsync()
+        { 
+        string URI = "http://localhost:49976/api/stocks";
+        var templist=await GetStockInformation(URI);
+        return templist;
+        }
+
+
+        public async Task<List<Stock>> GetStockInformation(string url)
+        {
+            List<Stock> stockList = new List<Stock>();
+            List<Stock> dupList = new List<Stock>();
+
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var stockJson = await response.Content.ReadAsStringAsync();
+
+                        stockList = JsonConvert.DeserializeObject<Stock[]>(stockJson).ToList();
+                        return stockList;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ logicAPI/logicAPI/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logicAPI
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public System.DateTime StockDate { get; set; }
+        public string Symbol { get; set; }
+        public string OpenStock { get; set; }
+        public string CloseStock { get; set; }
+        public string LowStock { get; set; }
+        public string HighStock { get; set; }
+        public string Volume { get; set; }
+    }
+}
